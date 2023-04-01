a3831c5a8a2ec4824474b1d875a4806c8a9fd113
dkd1999 <dkdanam@gmail.com>
2023-04-02 01:37:31 +0530

    up7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+  
+    extend: {
+      colors: {
+        gry : '#e4eddb',
+        purp: '#5b446a',
+        yelwsh : '#e6f18c',
+        cyandark : '#faffb8',
+        orang:'#ff5722',
+        yelwsh1:'#fec100',
+        unk:'#ff9de2'
+
+
+     },
+    },
   },
   plugins: [ ],
 }
